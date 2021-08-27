15d994418e7a2a8c1d355587c13d139ff2f3410a
Aamil Ali <aamil.ali@aamilr103.successivetech.com>
2021-08-27 20:06:09 +0530

    docs: diamond and equilateral patterns

---
+++ extra/patterns/diamond.js
@@ -0,0 +1,30 @@
+n=process.argv[2];
+function diamond(a)
+{
+    let string = "";
+    console.log("Print a diamond with rows "+a);
+    for (let i = 1; i <= a; i++)
+    {
+        for (let j = a; j > i; j--)
+        {
+            string += " ";
+        }
+        for (let k = 1; k <= i; k++)
+        string += "* ";
+        string += "\n";
+    }
+    for (var i = 0; i < a; i++)
+    {
+        for (var j = i; j > 0; j--)
+        {
+            string += " ";
+        }
+        for (var k = i; k < a; k++)
+        {
+            string += "* ";
+        }
+        string += "\n";
+    }
+    console.log(string);
+}
+diamond(n);
---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,18 @@
+function equilateral (a)
+{
+    let string = "";
+    console.log("Print a equilateral with rows "+a);
+
+for (let i = 1; i <= a; i++)
+{
+    for (let j = a; j > i; j--)
+    {
+        string += " ";
+    }
+    for (let k = 1; k <= i; k++)
+    string += "* ";
+    string += "\n";
+}
+console.log(string);
+}
+equilateral(process.argv[2]);
