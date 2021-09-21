14f6763ed94286fa004f6b360c9bba63d268af65
Aamil Ali <aamil.ali@aamilr103.successivetech.com>
2021-09-21 21:43:42 +0530

    feat: Authentication and Authorization

---
+++ src/libs/routes/authMiddleWare.ts
@@ -0,0 +1,52 @@
+import * as jwt from 'jsonwebtoken';
+import configuration from '../../config/configuration';
+import hasPermission from '../hasPermission';
+
+const authMiddleWare = (module, permissionType) => (req, res, next) => {
+  const token = req.header('Authorization');
+  console.log('Token:', token);
+
+  if (!token) {
+    next({
+      error: 'Unauthorized Access',
+      message: 'Token not found!',
+      status: 403,
+    });
+  }
+
+  const { secret } = configuration;
+  console.log('Key:', secret);
+
+  let user;
+  try {
+    user = jwt.verify(token, secret);
+  } catch (error) {
+    next({
+      error: 'Unauthorized Access',
+      message: 'User not Authorized!',
+      status: 403,
+    });
+  }
+  console.log('User:', user);
+
+  if (!user) {
+    next({
+      error: 'Unauthorized Access',
+      message: 'User not Authorized!',
+      status: 403,
+    });
+  }
+
+  if (!hasPermission(module, user.role, permissionType)) {
+    next({
+      error: 'Unauthorized Access',
+      message: 'Permission Denied!',
+      status: 403,
+    });
+  }
+
+  req.user = user;
+  next();
+};
+
+export default authMiddleWare;
